bf14fda2fe107f591eea1922541d7b2cd7372883
praveen-gosula <gpraveen342@gmail.com>
2023-07-24 11:39:04 +0000

    Home page added

---
+++ reactapp/src/components/homePage/Footer.jsx
@@ -0,0 +1,12 @@
+import "../../style/home_style/footer.css"
+
+const Footer = () => {
+  return (
+    <div className="mail">
+      <h1 className="mailTitle">Save time, save money!</h1>
+      
+    </div>
+  )
+}
+
+export default Footer;
