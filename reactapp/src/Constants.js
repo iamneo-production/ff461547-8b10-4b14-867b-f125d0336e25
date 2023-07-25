47131c8c46e6659c86b98a4bbb8d50d09e75665a
Logeshwari2002 <logupriya1568@gmail.com>
2023-07-25 07:51:54 +0000

    flight style changes

--- reactapp/src/Constants.js
+++ reactapp/src/Constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://8080-bccfbccaacabcefaffbedbdcceeaeaadbdbabf.project.examly.io';
+export const baseUrl = 'https://8080-deaeeacfeaecfaefaffbedbdcceeaeaadbdbabf.project.examly.io/';
 
  export const Hotel={
      ROOM_SIZE:4,
