cd76a25e4caaf363712f5d676177343c921e0d60
praveen-gosula <gpraveen342@gmail.com>
2023-07-26 11:01:50 +0000

    cmt66

---
+++ reactapp/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0 0 25px 0 rgba(0, 0, 0, .3);',
+      }
+    },
+  },
+  plugins: [],
+}
+
