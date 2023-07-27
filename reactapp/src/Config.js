4993678596d0d65e2ec339369fb13f689f971734
Mohanhrx <mohankumarcsbs@gmail.com>
2023-07-27 07:45:21 +0000

    config undo

--- reactapp/src/Config.js
+++ reactapp/src/Config.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://8080-bcdccfbefaffbedbdcceeaeaadbdbabf.project.examly.io';
+export const baseUrl = 'https://8080-dfdddfcaffbedfadefaffbedbdcceeaeaadbdbabf.project.examly.io';
 
 //RazorPay Keyshttps:
 export const RazorPay = {
