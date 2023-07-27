501b41726c13129966b1878d877d5e1671457e1c
varshitha89 <varshithabm9743@gmail.com>
2023-07-27 09:51:31 +0000

    saving hotelresult changes

--- reactapp/src/components/hotel/hotelresult/HotelResult.jsx
+++ reactapp/src/components/hotel/hotelresult/HotelResult.jsx
@@ -7,17 +7,18 @@ const HotelResults = () => {
       className="rightSide container basis-2/3 bg-[#fff] p-[0.7rem]
         overflow-y-auto overflow-hidden scroll-smooth hover:scroll-auto
         rounded-[18px] border-[1px] border-solid shadow-3xl">
-    <div className="card">
+    <div className="hotel-card">
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTg5hXGv_ut5tItRx_6gOS_u-JjsoWY143YQ&usqp=CAU"
         alt=""
         width="150px"
         height="200px"
       />
+      <br></br>
       <h1 className="head-hotel">Royal Home Stay</h1>
       <p>Country: India</p>
       <p>City: Delhi</p>
-      <p>Price per Day: $6000</p>
+      <p>Price per Day: $600</p>
       <p>Rating: 4.2</p>
       <p>Number of Ratings: 2</p>
       <button className="button-hotel">Book Now</button>
