68efd2c6de04b7f20aebaf6a0d892b0016dab2a9
arijit-das18 <das181100arijit@gmail.com>
2023-07-31 07:13:59 +0000

    final commit

--- reactapp/src/containers/SignIn.jsx
+++ reactapp/src/containers/SignIn.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 import React, {useEffect} from 'react';
 import Login from '../components/Auth/SignIn';
->>>>>>> 1655f8e46693827b5a991949bce50d7584d091d5
 
 function SignIn() {
     useEffect(()=>{
